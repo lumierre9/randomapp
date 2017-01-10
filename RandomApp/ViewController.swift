18b47f9efe285a9a319ad3edc7819b16f7eb64a4
Yonatan Andy Fajar Nugraha <lumierre9@i.softbank.jp>
2017-01-10 22:40:27 +0900

    Initial Commit

---
+++ RandomApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RandomApp
+//
+//  Created by Yonatan Andy Fajar Nugraha on 2017/01/10.
+//  Copyright © 2017 Andy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
